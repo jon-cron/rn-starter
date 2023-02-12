bd60cbc9750bb7a9849f6230caeb9d12666e7ec8
jon-cron <jonathanacm95@gmail.com>
2023-02-11 22:12:12 -0700

    second useState word is a banana word

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -4,17 +4,18 @@ import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
 
 
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  // NOTE variable, changes variable and reactively changes on screen
+  const [counter, changeCount] = useState(0);
 
   return (
     <View>
       <Button title="Increase"
       onPress={()=>{
-        setCounter(counter + 1)
+        changeCount(counter + 1)
       }}/>
       <Button title="Decrease"
             onPress={()=>{
-              setCounter(counter - 1)
+              changeCount(counter - 1)
             }}/>
       <Text>Current Count: {counter}</Text>
     </View>
