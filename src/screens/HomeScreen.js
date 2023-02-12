1014b9f935cd233bc4c3f8ebb25f1c5c1912d3b8
jon-cron <jonathanacm95@gmail.com>
2023-02-11 17:30:35 -0700

    push

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,28 @@
+// FIXME if you are having troubling running your project try "expo start --tunnel" in the CMP
+
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = props => {
-  console.log(props)
+// NOTE by instead of taking the params "props" we are drilling into that param by using navigation. Now we only have to do navigation.navigate instead of props.navigation.navigate
+// const HomeScreen = props => {
+const HomeScreen = ({ navigation }) => {
   return (
   <View>
     <Text style={styles.textStyle}>Home Screen</Text>
     {/* NOTE buttons are self closing and must have a title */}
     <Button
     // NOTE onPress invokes a function
-      onPress={()=> console.log("button press")}
+      onPress={()=> navigation.navigate('Components')}
       title="Go To Components Demo"/>
-      <TouchableOpacity
-        onPress={()=> console.log("go to list")}>
+    <Button
+    // NOTE onPress invokes a function
+      onPress={()=> navigation.navigate('List')}
+      title="Go To List Demo"/>
+      {/* NOTE touchableOpacity will be used more so than actual buttons */}
+      {/* <TouchableOpacity
+        onPress={()=> props.navigation.navigate('List')}>
         <Text>Go To List Demo</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
   </View>
     );
 };
